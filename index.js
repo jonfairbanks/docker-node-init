a012dc06cb0b910845c33b7522d1d3507ddd66ab
Jon Fairbanks <fairplay89@gmail.com>
2021-08-09 16:48:49 -0700

    Prettier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Setup Express
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 const app = require('./app.js')
 
 // Launch the App
-app.listen(PORT, '0.0.0.0');
-console.log(`docker-node-init is up and running...`); // eslint-disable-line
+app.listen(PORT, '0.0.0.0')
+console.log(`docker-node-init is up and running...`) // eslint-disable-line
